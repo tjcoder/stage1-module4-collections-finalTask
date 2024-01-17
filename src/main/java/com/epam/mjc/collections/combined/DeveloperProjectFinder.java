425c77d5aea6cf88646995c46aeaf6746b631495
Taalay Joldoshev <gmoftj@gmail.com>
2024-01-17 18:08:45 +0600

    Done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,30 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> pair : projects.entrySet()) {
+            String projectName = pair.getKey();
+            Set<String> developers = pair.getValue();
+
+            if (developers.contains(developer)) {
+                list.add(projectName);
+            }
+        }
+
+        list.sort((o1, o2) -> {
+            Integer l1 = o1.length();
+            Integer l2 = o2.length();
+
+            if (l1.equals(l2)) {
+                return o2.compareTo(o1);
+            } else {
+                return l2.compareTo(l1);
+            }
+        });
+
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,25 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (String s : sourceMap.keySet()) {
+            int l = s.length();
+            Set<String> set;
+            if (!map.containsKey(l)) {
+                set = new HashSet<>();
+            } else {
+                set = map.get(l);
+            }
+            set.add(s);
+            map.put(l, set);
+        }
+
+        return map;
     }
 }
